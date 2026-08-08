b8faac8f19dd0ef6315640281b32282e0f0772f6
liliziv2 <zhonglululuzhong@outlook.com>
2026-08-08 19:19:11 +0800

    feat: add wallpaperRes to PearlTideTheme

--- app/src/main/java/me/rerere/rikkahub/ui/theme/presets/custom/PearlTideTheme.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/theme/presets/custom/PearlTideTheme.kt
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * 橘瓣 OrangeChat
  * 衍生自 RikkaHub (https://github.com/rikkahub/rikkahub)，原作者 RE
  * 本项目基于 GNU AGPL v3 开源，详见根目录 LICENSE 文件
@@ -22,6 +22,7 @@ val PearlTideThemePreset by lazy {
         },
         standardLight = lightScheme,
         standardDark = darkScheme,
+        wallpaperRes = R.drawable.wall_pearltide,
     )
 }
 
